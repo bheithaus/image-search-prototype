73f62baf08c1dc5619f77434e95d188906170087
Brian Heithaus <bheithaus@gmail.com>
2020-12-30 17:34:23 -1000

    fix /api/search error handling

--- pages/api/search.js
+++ pages/api/search.js
@@ -3,18 +3,16 @@ import axios from 'axios'
 import autocorrect from '../../lib/autocorrect'
 
 const SEARCH_API_URL = 'https://api.flickr.com/services/rest'
+const TEAPOT_ERROR_CODE = 418
+const SUCCESS_CODE = 200
 
+// do autocorrect algorithm
 function correctQuery (query) {
-  // remove non alphanumeric characters
-  let cleanedQuery = query.replace(/[^0-9a-z]/gi, '')
-  console.log('cleaned query', cleanedQuery)
-
-  // do autocorrect algorithm
   let corrected
   try {
-    corrected = autocorrect(cleanedQuery)
+    corrected = autocorrect(query)
   } catch (e) {
-    console.log(e)
+    console.error(e)
   }
 
   return corrected
@@ -27,7 +25,7 @@ export default function SearchRequestHandler (req, res) {
   corrected = correctQuery(corrected)
 
   if (!corrected) {
-    res.statusCode = 500
+    res.statusCode = TEAPOT_ERROR_CODE
     return res.json({ error: 'autocorrect not possible for your query' })
   }
 
@@ -38,29 +36,28 @@ export default function SearchRequestHandler (req, res) {
       format: 'json',
       nojsoncallback: 1,  // request raw json response
       text: corrected,
-      api_key: process.env.FLICKR_KEY
+      api_key: process.env.FLICKR_KEY,
+      per_page: 25
     }
   }
 
-  console.log('3rd party API request url:', SEARCH_API_URL)
-  console.log('3rd party API request params:', requestParams)
+  console.log('3rd party API request url: ', SEARCH_API_URL)
+  console.log('3rd party API request params: ', requestParams)
 
   axios.get(SEARCH_API_URL, requestParams)
   .then((response) => {
-    console.log('FLICKR API response', response)
-
-    if (response.status == 200) {
-      res.statusCode = 200
+    // console.log('FLICKR API response :', response)
+    if (response.status == SUCCESS_CODE) {
+      res.statusCode = SUCCESS_CODE
       // just return the 1st page for now
       res.json({ photos: response.data.photos.photo, query: corrected })
     } else {
       throw new Error("no results")
     }
-    // $("#output").append("<img src='" + data.images[i].display_sizes[0].uri + "'/>");
   })
   .catch((error) => {
-    console.log(error)
-    res.statusCode = 500
+    console.error(error)
+    res.statusCode = TEAPOT_ERROR_CODE
     res.json({ error: error })
   })
 }
